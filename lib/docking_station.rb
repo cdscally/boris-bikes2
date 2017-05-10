baf09b596078a4e0ac4a025cb1e1716238e6ec3a
Sam Pritchard <sampritchard@msn.com>
2017-05-10 15:52:04 +0100

    merge

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,10 +4,6 @@ class DockingStation
 
   attr_reader :bike
 
-<<<<<<< HEAD
-=======
-
->>>>>>> c8c5a28db4a62df16ed4e102d2ae33e3f5ed391e
   def release_bike
     raise 'No bikes available' unless @bike
     @bike
@@ -17,9 +13,4 @@ class DockingStation
     @bike = bike
   end
 
-<<<<<<< HEAD
-  def docking?(bike)
-  end
-=======
->>>>>>> c8c5a28db4a62df16ed4e102d2ae33e3f5ed391e
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,47 +1,26 @@
-require ‘docking_station’
+require 'docking_station'
 
 describe DockingStation do
-<<<<<<< HEAD
+
  it { is_expected.to respond_to :release_bike}
-end
 
-describe DockingStation do
- it “docking station to release bike” do
-   expect(subject.release_bike).to be_working
- end
-=======
-  it { is_expected.to respond_to :release_bike}
-
-
-  # it "docking station to release bike" do
-  #   expect(subject.release_bike).to be_working
-  # end
+ # it “docking station to release bike” do
+ #   expect(subject.release_bike).to be_working
+ # end
 
   it { is_expected.to respond_to(:dock_bike).with(1).argument}
   it { is_expected.to respond_to(:bike)}
->>>>>>> c8c5a28db4a62df16ed4e102d2ae33e3f5ed391e
 
- it { is_expected.to respond_to(:dock_bike).with(1).argument}
- it { is_expected.to respond_to(:bike)}
-
- it “docks something” do
+ it 'docks something' do
    bike = Bike.new
    expect(subject.dock_bike(bike)).to eq bike
  end
 
- it “returns bikes at dock station” do
+ it 'returns bikes at dock station' do
    bike = Bike.new
    subject.dock_bike(bike)
    expect(subject.bike).to eq bike
  end
-
-<<<<<<< HEAD
-=======
-  it "returns bikes at dock station" do
-    bike = Bike.new
-    subject.dock_bike(bike)
-    expect(subject.bike).to eq bike
-  end
 
   describe '#release_bike' do
     it 'releases a bike' do
@@ -56,6 +35,4 @@ describe DockingStation do
       expect{subject.release_bike}.to raise_error 'No bikes available'
     end
   end
-
->>>>>>> c8c5a28db4a62df16ed4e102d2ae33e3f5ed391e
 end
